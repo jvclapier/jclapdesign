bfdddbe55d6adca9f52670e219369f6eb0b53ac8
Jess Clapier <jvclapier@gmail.com>
2018-09-28 10:07:43 -0600

    starting my work section

--- portfolio/homepage/static/homepage/index.js
+++ portfolio/homepage/static/homepage/index.js
@@ -1,1 +1,39 @@
+(function ($) {
+  $(document).ready(function(){
 
+    // hide .navbar first
+    $(".navbar").hide();
+
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+
+            // set distance user needs to scroll before we start fadeIn
+            if ($(this).scrollTop() > 400) {
+                $('.navbar').fadeIn();
+                console.log("reached 400 px scroll");
+            } else {
+                $('.navbar').fadeOut();
+                console.log("back to the top scroll");
+            }
+        });
+    });
+
+});
+  }(jQuery));
+
+  $( document ).ready(function() {
+
+    // Open navbarSide when button is clicked
+    $('#navbarSideButton').on('click', function() {
+      $('#navbarSide').addClass('reveal');
+      $('.overlay').show();
+    });
+
+    // Close navbarSide when the outside of menu is clicked
+    $('.overlay').on('click', function(){
+      $('#navbarSide').removeClass('reveal');
+      $('.overlay').hide();
+    });
+
+  });
