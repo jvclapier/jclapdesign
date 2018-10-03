222ffd111255ca2131e16c39dfce001b13a35e7b
Jess Clapier <jvclapier@gmail.com>
2018-10-03 14:56:07 -0600

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,7 +29,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
+    '127.0.0.1',
     '10.37.145.164',
+    'https://jclapdesign.herokuapp.com/',
+    'jclapdesign.herokuapp.com/',
 ]
 
 
@@ -80,11 +85,11 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'jclapdesign',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': os.environ['H_DATABASE_NAME'],
+        'USER': os.environ['H_DATABASE_USER'],
+        'PASSWORD': os.environ['H_DATABASE_PASSWORD'],
+        'HOST': os.environ['H_DATABASE_HOST'],
+        'PORT': '5432',
     }
 }
 
